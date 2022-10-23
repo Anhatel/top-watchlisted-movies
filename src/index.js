b6a27286586870b849ae9f815797f799f1101bd6
Dejan <mjtvc93@gmail.com>
2022-10-23 14:30:12 +0200

    Added changes related to sorting movies and users list.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,40 @@ export default class MoviesAnalyzer {
     topWatchlistedMoviesAmongFriends(userId) {
         // TODO: Implement
 
-        return [];
+        const providedUserInfo = this.users.find(({ userId: idOfUser }) => userId === idOfUser);
+
+        const { friends: providedUserFriendsList } = providedUserInfo;
+        let moviesAndWatchListCount = {};
+
+        this.movies.forEach((movie) => {
+            const { title, watchlist } = movie;
+            let watchListCount = 0;
+
+            watchlist.forEach((watchListUserId) => {
+                if (providedUserFriendsList?.includes(watchListUserId)) {
+                    if (moviesAndWatchListCount[title]) {
+                        moviesAndWatchListCount[title] = {
+                            title,
+                            watchListCount: watchListCount += 1
+                        }
+                    } else {
+                        moviesAndWatchListCount[title] = {
+                            title,
+                            watchListCount: watchListCount += 1
+                        }
+                    }
+                }
+            });
+        });
+
+        return Object.values(moviesAndWatchListCount)
+                .sort((firstMovie, secondMovie) => secondMovie.watchListCount - firstMovie.watchListCount)
+                .slice(0, 4)
+                .sort((firstMovie, secondMovie) => {
+                    if (firstMovie.watchListCount === secondMovie.watchListCount) {
+                        return firstMovie.title.localeCompare(secondMovie.title);
+                    }
+                })
+                .map(({ title }) => title);
     }
 }
